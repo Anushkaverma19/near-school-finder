4bc983a2edd89857f9c22ec404515ab8ad2b8bf9
unknown <10anushka2023@gmail.com>
2026-07-24 01:40:27 +0530

    Enable auth debug

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,6 @@ import Google from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb-client";
 
-
 export const {
   handlers,
   auth,
@@ -11,65 +10,35 @@ export const {
   signOut,
 } = NextAuth({
 
+  debug: true, // 👈 यह line add करो
+
   adapter: MongoDBAdapter(clientPromise),
 
-
   providers: [
-
     Google({
-
       clientId: process.env.AUTH_GOOGLE_ID!,
-
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-
     }),
-
   ],
-
-
 
   trustHost: true,
 
-
   secret: process.env.AUTH_SECRET,
 
-
-
   session: {
-
     strategy: "database",
-
   },
-
-
 
   pages: {
-
     signIn: "/",
-
   },
-
-
 
   callbacks: {
-
-
     async session({ session, user }) {
-
-
       if (session.user) {
-
         session.user.id = user.id;
-
       }
-
-
       return session;
-
     },
-
-
   },
-
-
 });
