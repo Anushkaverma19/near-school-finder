c72c7db9b43b6776f8593e76abcca7c71b45b230
unknown <10anushka2023@gmail.com>
2026-07-23 11:51:14 +0530

    fix mongoose type for vercel

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -1,41 +1,61 @@
 import mongoose from "mongoose";
 
+
 const MONGODB_URI = process.env.MONGODB_URI;
 
+
 if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI in .env.local");
+  throw new Error("Please add MONGODB_URI in .env");
 }
 
-let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null,
-  };
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
 }
+
+
+const globalWithMongoose = globalThis as typeof globalThis & {
+  mongoose?: MongooseCache;
+};
+
+
+
+let cached: MongooseCache = globalWithMongoose.mongoose || {
+  conn: null,
+  promise: null,
+};
+
+
+
+globalWithMongoose.mongoose = cached;
+
+
 
 async function connectDB() {
+
 
   if (cached.conn) {
     return cached.conn;
   }
 
 
+
   if (!cached.promise) {
 
-    cached.promise = mongoose.connect(MONGODB_URI)
-      .then((mongoose) => {
-        return mongoose;
-      });
+    cached.promise = mongoose.connect(MONGODB_URI);
 
   }
 
 
+
   cached.conn = await cached.promise;
 
+
   return cached.conn;
+
 }
+
 
 
 export default connectDB;
